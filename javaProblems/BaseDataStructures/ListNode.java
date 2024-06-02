65d12d0cb12b1243e6194250180175b1c4f61ae6
dnavaria <dnavaria15@gmail.com>
2024-06-03 01:35:24 +0530

    LinkedList base datastructure added

---
+++ javaProblems/BaseDataStructures/ListNode.java
@@ -0,0 +1,12 @@
+package BaseDataStructures;
+
+public class ListNode {
+    public int val;
+    public ListNode next;
+
+    ListNode(int x) {
+        this.val = x;
+        this.next = null;
+    }
+
+}
