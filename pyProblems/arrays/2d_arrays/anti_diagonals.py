1731b3a8445838ec90aeb9423719a7583c30d9b7
dnavaria <dk17816@gmail.com>
2024-06-03 01:00:15 +0530

    modified anti-diagonals

--- pyProblems/arrays/2d_arrays/anti_diagonals.py
+++ pyProblems/arrays/2d_arrays/anti_diagonals.py
@@ -2,30 +2,33 @@ class Solution:
     # @param A : list of list of integers
     # @return a list of list of integers
     def diagonal(self, A):
-        i = 0
-        result = []
-        for j in range(len(A)):
-            s = i
-            e = j
-            arr = [0] * len(A)
-            index = 0
-            while s < len(A) and e >= 0:
-                arr[index] = A[s][e]
+        def traverse_downwards_diagonally(s, e, N, A):
+            diagonal = [0] * N
+            s = s
+            e = e
+            ptr = 0
+            while s < N and e >= 0:
+                diagonal[ptr] = A[s][e]
                 s += 1
                 e -= 1
-                index += 1
-            result.append(arr)
+                ptr += 1
+            return diagonal
 
-        j = len(A) - 1
-        for i in range(1, len(A)):
-            arr = [0] * len(A)
-            s = i
-            e = j
-            index = 0
-            while s < len(A) and e >= 0:
-                arr[index] = A[s][e]
-                s += 1
-                e -= 1
-                index += 1
-            result.append(arr)
+        result = []
+        N = len(A[0])
+
+        for j in range(N):
+            temp = traverse_downwards_diagonally(s=0, e=j, N=N, A=A)
+            result.append(temp)
+
+        for i in range(1, N):
+            temp = traverse_downwards_diagonally(s=i, e=N - 1, N=N, A=A)
+            result.append(temp)
         return result
+
+
+if __name__ == '__main__':
+    mat = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+    resultant_matrix = Solution().diagonal(A=mat)
+    for i in resultant_matrix:
+        print(*i)
